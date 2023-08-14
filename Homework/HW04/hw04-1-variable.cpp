f54dca7e1ab7bf12cad5d4a77b2252a7fabfe944
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-08-14 18:44:14 +0700

    4-1

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -1,14 +1,13 @@
-/*
-    จงแสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
+#include <stdio.h>
 
-    Output:
-        10 20.0 Haha a
-*/
-
+int main() {
     int a = 10;
     float b = 20.0;
     char c[] = "Haha";
     char d = 'a';
 
-    printf("%d, %f, %s, %c\n", a, b, c, d);
- 
+    // แสดงผลตัวแปร a, b, c, และ d ตามลำดับที่ระบุ
+    printf("%d %.1f %s %c\n", a, b, c, d);
+
+    return 0;
+}
